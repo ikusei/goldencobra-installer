1a8b5e0ee57aed879c21ed83e0ce9c872069c4c2
Marco Metz <marco.metz@gmail.com>
2013-01-17 17:57:12 +0100

    template file for rails new

---
+++ template.rb
@@ -0,0 +1,12 @@
+if ARGV[0] == "."
+  @gemset_name == system("pwd").to_s.split("/").last
+else
+  @gemset_name = ARGV[0]
+end
+if yes?("Would you like to configure rvm?")
+  @ruby_version = ask("What is your current ruby version (bsp: 1.9.3-p194)")
+  system("rvm #{@ruby_version} do rvm gemset create #{@gemset_name}")
+end
+gem('goldencobra', :git => 'ssh://git@git.ikusei.de:7999/GC/basis-modul.git')
+system("rvm #{@ruby_version}@#{@gemset_name} do bundle install")
+generate("goldencobra:install")
