583e0eb722e469cc3998fcd972cfcb74a14b1a51
Marco Metz <metz@ikusei.de>
2014-05-05 15:23:19 +0200

    Update template.rb

--- template.rb
+++ template.rb
@@ -9,5 +9,7 @@ if yes?("Would you like to configure rvm?")
 end
 gem('goldencobra', :git => 'git://github.com/ikusei/Goldencobra.git')
 gem('activeadmin', :git => 'git://github.com/ikusei/active_admin.git', :require => 'activeadmin')
+gem('acts-as-taggable-on', :git => 'git://github.com/mbleigh/acts-as-taggable-on')
+gem('meta-tags', :git => 'git://github.com/jazzgumpy/meta-tags.git')
 system("rvm #{@ruby_version}@#{@gemset_name} do bundle install")
 generate("goldencobra:install")
