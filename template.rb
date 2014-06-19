129ad9d6d13db24529f3f5fc832f81de89c44b95
5minpause <holger@5minutenpause.com>
2014-06-19 11:26:45 +0200

    Updates the template to the correct url
    My username changed. This change reflects the new username and updates the url accordingly.

--- template.rb
+++ template.rb
@@ -10,6 +10,6 @@ end
 gem('goldencobra', :git => 'git://github.com/ikusei/Goldencobra.git')
 gem('activeadmin', :git => 'git://github.com/ikusei/active_admin.git', :require => 'activeadmin')
 gem('acts-as-taggable-on', :git => 'git://github.com/mbleigh/acts-as-taggable-on')
-gem('meta-tags', :git => 'git://github.com/jazzgumpy/meta-tags.git')
+gem('meta-tags', :git => 'git://github.com/5minpause/meta-tags.git')
 system("rvm #{@ruby_version}@#{@gemset_name} do bundle install")
 generate("goldencobra:install")
